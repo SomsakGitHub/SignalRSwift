ed76c6837a9e27c5a9fc52f2528e590531119b49
Somsak Kaeworasan <somsak.kaeworasan@outlook.com>
2021-06-27 22:59:59 +0700

    First commit.

---
+++ SignalR-Swift/SignalR-Swift/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TestServer
+{
+    public class ChatHub : Hub
+    {
+        public Task Broadcast(string sender, string message)
+        {
+            if (message == "abort")
+            {
+                Context.Abort();
+                return Task.CompletedTask;
+            }
+
+            return Clients.All.SendAsync("NewMessage", sender, message);
+        }
+    }
+}
---
+++ SignalR-Swift/SignalR-Swift/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SignalR_Swift
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole()
+                        .SetMinimumLevel(LogLevel.Debug);
+                })
+                .UseUrls("http://0.0.0.0:5000/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
